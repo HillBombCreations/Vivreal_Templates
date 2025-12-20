9b8182cd92f3a5dd08746df53d3b78f834fcb70c
Justin Ceccarelli <justinceccarelli@gmail.com>
2025-12-19 17:31:04 -0800

    Removing site map for now

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,12 +1,19 @@
 import type { MetadataRoute } from 'next';
-import { getSiteMap } from '@/lib/api/siteData';
+import { headers } from 'next/headers'
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const siteMap = await getSiteMap();
+const handleBuildUrl = async (type: string) => {
+  const h = await headers();
+  const host = h.get("x-forwarded-host") ?? h.get("host")!;
+  const proto = h.get("x-forwarded-proto") ?? "https";
+  const base = `${proto}://${host}`;
+  const url = new URL(`${base}/api/${type}`);
+  return url;
+};
 
-  if (Array.isArray(siteMap)) {
-    return siteMap;
-  }
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   if (Array.isArray(siteMap)) {
+//     return siteMap;
+//   }
 
-  return [];
-}
+//   return [];
+// }
